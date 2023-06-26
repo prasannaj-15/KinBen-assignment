d5dffadaa0284c3b03e3183d103531fed83fa0b0
Suhaib Malik <suhaibmalik6397@gmail.com>
2023-06-26 20:44:20 +0530

    updated app config

--- kinben/src/main/java/com/kinben/Controller/AdminController.java
+++ kinben/src/main/java/com/kinben/Controller/AdminController.java
@@ -2,15 +2,25 @@ package com.kinben.Controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("*")
 public class AdminController {
     
 
     @GetMapping("/admin/signin")
     public ResponseEntity<String> login(Authentication auth){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String authToken = authentication.getCredentials().toString();
+
+        System.out.println("Something");
+        System.out.println(authToken);
+
+
         return ResponseEntity.ok("Login Successfull");
     }
 
